30d48eeb692c72af56e68ee6db56befcca9a1bb5
Edward Barry <ed.m.barry@gmail.com>
2018-06-24 23:00:15 +1000

    initial commit of chris' asmFoam repo

---
+++ asmFoam.C
@@ -0,0 +1,166 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    asmFoam
+
+Description
+    Solver for an activated sludge system based on ASM1 biokinetics.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "multiphaseSystem.H"
+#include "phaseModel.H"
+#include "dragModel.H"
+#include "heatTransferModel.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "pimpleControl.H"
+#include "IOMRFZoneList.H"
+#include "CorrectPhi.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+    #include "createAsmFields.H"
+    #include "readBiokineticsProperties.H"
+    #include "initContinuityErrs.H"
+    #include "createTimeControls.H"
+    #include "correctPhi.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+    scalar slamDampCoeff
+    (
+        fluid.lookupOrDefault<scalar>("slamDampCoeff", 1)
+    );
+
+    dimensionedScalar maxSlamVelocity
+    (
+        "maxSlamVelocity",
+        dimVelocity,
+        fluid.lookupOrDefault<scalar>("maxSlamVelocity", GREAT)
+    );
+
+    #include "liquidPhase.H"
+
+    turbulence->validate();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+        #include "CourantNo.H"
+        #include "setDeltaT.H"
+
+        runTime++;
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+            turbulence->correct();
+            fluid.solve();
+            rho = fluid.rho();
+            #include "zonePhaseVolumes.H"
+
+            //#include "TEqns.H"
+            #include "UEqns.H"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                #include "pEqn.H"
+            }
+
+            #include "DDtU.H"
+        }
+
+        #include "asmRates.H"
+
+        #include "SSEqn.H"
+        SSEqn.relax();
+        SSEqn.solve();
+
+        #include "XSEqn.H"
+        XSEqn.relax();
+        XSEqn.solve();
+
+        #include "XBHEqn.H"
+        XBHEqn.relax();
+        XBHEqn.solve();
+
+        #include "XBAEqn.H"
+        XBAEqn.relax();
+        XBAEqn.solve();
+
+        #include "XPEqn.H"
+        XPEqn.relax();
+        XPEqn.solve();
+
+        #include "SOEqn.H"
+        SOEqn.relax();
+        SOEqn.solve();
+
+        #include "SNOEqn.H"
+        SNOEqn.relax();
+        SNOEqn.solve();
+
+        #include "SNHEqn.H"
+        SNHEqn.relax();
+        SNHEqn.solve();
+
+        #include "SNDEqn.H"
+        SNDEqn.relax();
+        SNDEqn.solve();
+
+        #include "XNDEqn.H"
+        XNDEqn.relax();
+        XNDEqn.solve();
+
+        runTime.write();
+
+        Info<< "ExecutionTime = "
+            << runTime.elapsedCpuTime()
+            << " s\n\n" << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
